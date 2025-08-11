65c1b7b828ac1af7409227bf3baebe5c6766cabf
daniel.gaerber <daniel.gaerber@main-sequence.io>
2025-08-11 18:41:50 +0200

    fix: run all

---
+++ data_connectors/scripts/run_all_source.py
@@ -0,0 +1,24 @@
+from data_connectors.prices.alpaca.time_series import AlpacaEquityBars
+from data_connectors.fundamentals.coingecko.crypto_fundamentals import CoinGeckoMarketCap
+from data_connectors.fundamentals.polygon.equity_fundamentals import PolygonQFundamentals
+from data_connectors.prices.binance.time_series import BinanceHistoricalBars, TimeBarConfig
+from data_connectors.fundamentals.polygon.equity_fundamentals import PolygonDailyMarketCap
+from data_connectors.scripts.create_translation_tables import create_asset_translation_table
+
+ts = PolygonDailyMarketCap(asset_list=None)
+ts.run(debug_mode=True, force_update=True)
+
+ts = CoinGeckoMarketCap(asset_list=None)
+ts.run(debug_mode=True, force_update=True)
+
+ts = PolygonQFundamentals(asset_list=None)
+ts.run(debug_mode=True, force_update=True)
+
+ts = AlpacaEquityBars(asset_list=None, frequency_id="1d", adjustment="all")
+ts.run(debug_mode=True, update_tree=True, force_update=True)
+
+ts = BinanceHistoricalBars(asset_category_identifier="top_50_crypto_market_cap", asset_list=None, bar_configuration=TimeBarConfig(frequency_id="1d"))
+ts.run(debug_mode=True, update_tree=True, force_update=True)
+
+
+create_asset_translation_table()
