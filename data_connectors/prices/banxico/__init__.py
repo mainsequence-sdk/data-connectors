cad84b87f87e5658cf8bc0c4ea27ef0714afee63
Jose Antonio Alatorre <jose@main-sequence.io>
2025-10-09 12:15:54 -0700

    Fixed banxico source

--- data_connectors/prices/banxico/__init__.py
+++ data_connectors/prices/banxico/__init__.py
@@ -1,24 +1,8 @@
 
 from mainsequence.client import Constant as _C
-
 from .builders import *
+from .settings import *
 
-
-constants_to_seed = dict(
-    REFERENCE_RATE__TIIE_28="TIIE_28",
-    REFERENCE_RATE__TIIE_91="TIIE_91",
-    REFERENCE_RATE__TIIE_182="TIIE_182",
-    REFERENCE_RATE__TIIE_OVERNIGHT="TIIE_OVERNIGHT",
-
-    REFERENCE_RATE__CETE_28="CETE_28",
-    REFERENCE_RATE__CETE_91="CETE_91",
-    REFERENCE_RATE__CETE_182="CETE_182",
-
-    #curves
-    ZERO_CURVE__BANXICO_M_BONOS_OTR = "BANXICO_M_BONOS_OTR",
-)
-
-_C.create_constants_if_not_exist(constants_to_seed)
 
 
 
