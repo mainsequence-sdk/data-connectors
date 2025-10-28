1ab892fb18e274ad5c5343664b4bdc8348f11d8f
dev_main_sequence <dev.github@main-sequence.io>
2025-10-28 16:48:04 +0100

    hf discount curves

--- scripts/update_vector_valmer.py
+++ scripts/update_vector_valmer.py
@@ -1,11 +1,30 @@
 from src.data_connectors.prices.valmer.time_series import ImportValmer
 BUCKET_NAME = "Vector de precios"
 
-for i in range(360//5):
+
+
+first_time_update_loop=False
+ts_all_files = ImportValmer(
+    bucket_name=BUCKET_NAME,
+)
+try:
+    us = ts_all_files.get_update_statistics()
+except AttributeError:
+# first time update
+    first_time_update_loop = True
+if first_time_update_loop ==True:
+    for i in range(360//5):
+        ts_all_files = ImportValmer(
+            bucket_name=BUCKET_NAME,
+        )
+        ts_all_files.run(
+            debug_mode=True,
+            force_update=True,
+        )
+else:
     ts_all_files = ImportValmer(
         bucket_name=BUCKET_NAME,
     )
-
     ts_all_files.run(
         debug_mode=True,
         force_update=True,
